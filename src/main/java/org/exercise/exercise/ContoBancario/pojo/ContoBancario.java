6566ce9eaa7a2ac17f27b4205b94e6a1568ea378
CarloExtendsPersona <carlomelluso1995@gmail.com>
2024-07-05 14:22:12 +0200

    Database's working

--- src/main/java/org/exercise/exercise/ContoBancario/pojo/ContoBancario.java
+++ src/main/java/org/exercise/exercise/ContoBancario/pojo/ContoBancario.java
@@ -1,6 +1,16 @@
 package org.exercise.exercise.ContoBancario.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ContoBancario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private int saldo;
 
